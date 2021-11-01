faad0317ef1bb8e75968e95b5d15f11240e156aa
wjswotmd2 <wjswotmd2@gmail.com>
2021-11-01 16:59:39 +0900

    1

---
+++ Example/src/Example01/gugudan.java
@@ -0,0 +1,23 @@
+package Example01;
+
+import java.util.Scanner;
+
+public class gugudan {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("구구단의 시작단 >>");
+        int start = scan.nextInt();
+        System.out.print("구구단의 끝단 >>");
+        int end = scan.nextInt();
+
+        for(int i = 1; i <= 9; i++) {
+            for(int j = start; j <= end; j++) {
+                System.out.print(j + " X " + i + " = " + i * j + "\t");
+            }
+            System.out.println();
+
+        }
+    }
+}
