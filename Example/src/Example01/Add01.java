46ee420573887aabc0c63e614c6bee3d75d09cd1
wjswotmd2 <wjswotmd2@gmail.com>
2021-11-01 18:00:16 +0900

    123

---
+++ Example/src/Example01/Add01.java
@@ -0,0 +1,22 @@
+package Example01;
+
+import java.util.Scanner;
+
+public class Add01 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int num = scan.nextInt();
+
+        String s = scan.next();
+
+        int Sum = 0;
+
+        for(int i = 0; i < num; i++){
+            Sum += s.charAt(i) - '0';
+        }
+        System.out.println("총합 : " + Sum);
+    }
+}
--- Example/src/Example01/star01.java
+++ Example/src/Example01/star01.java
@@ -4,9 +4,10 @@ public class star01 {
     public static void main(String[] args) {
 
         for(int i = 0; i < 5; i++){
-            for(int j = 0; j < 5; j++) {
-
+            for(int j = 0; j <= i; j++) {
+                System.out.print("*");
             }
+            System.out.println();
         }
     }
 }
