4efe3b965eefdf88d68a36916a241623f2459e83
wjswotmd2 <wjswotmd2@gmail.com>
2021-12-06 17:48:42 +0900

    12/06

---
+++ Algorithm/src/algorithm02_12_06/algorithm02_12_05/BaekJonn2588.java
@@ -0,0 +1,31 @@
+package algorithm02_12_06.algorithm02_12_05;
+
+import java.util.Scanner;
+
+public class BaekJonn2588 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int num1 = scan.nextInt();
+        String num2 = scan.next();
+        int sum = 0;
+        String a = "";
+        int i = num2.length();
+
+        while (true) {
+            a = String.valueOf(num2.charAt(i-1));
+            sum = num1 * Integer.parseInt(a);
+            System.out.println(sum);
+            i--;
+            if(i == 0) {
+                System.out.println(num1 * Integer.parseInt(num2));
+                break;
+            }
+        }
+
+
+
+
+    }
+}
